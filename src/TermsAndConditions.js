6dc24fd642a2d0bc8c9bbbbf29dfd3ebf3741f34
AdamCernohous <71689826+AdamCernohous@users.noreply.github.com>
2023-05-04 17:40:21 +0200

    terms and conditions

---
+++ src/TermsAndConditions.js
@@ -0,0 +1,15 @@
+const TermsAndConditions = () => {
+  return (
+    <div className="App">
+      <h2>Tripzy Terms & Conditions</h2>
+        <ul>
+          <li><strong>Data Collection:</strong> We will only collect the personal data necessary for the proper functioning of the app. This may include information such as your name, email, and location (location is stored only temporarily).</li>
+          <li><strong>Use of Data:</strong> We will use your data solely for the purpose of providing our services to you and to improve the functionality of our app. We will not use your data for marketing purposes without your explicit consent.</li>
+          <li><strong>Sharing of Data:</strong> We will not share your data with any third-party service providers. We will not share your data with legal authorities unless required by law or to protect our rights.</li>
+          <li><strong>Security:</strong> We will take appropriate measures to protect your data from unauthorized access, disclosure, alteration, or destruction.</li>
+        </ul>
+    </div>
+  );
+}
+ 
+export default TermsAndConditions;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import TermsAndConditions from './TermsAndConditions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/app/:userToken" element={<App />} />
+        <Route path="/termsandconditions" element={<TermsAndConditions />} />
       </Routes>
     </Router>
   </React.StrictMode>
